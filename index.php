0f7341d1ec6929a26dc301e14696426580f0bda6
Teguh Bagas Mardiansyah <116806781+TeguhBagasM@users.noreply.github.com>
2025-08-08 16:13:39 +0300

    fix: bug error deployment

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Static files
-if (file_exists($_SERVER['DOCUMENT_ROOT'].$_SERVER['REQUEST_URI'])) {
-    return false;
-}
-
-// Laravel bootstrap
-require_once __DIR__ . '/public/index.php';
