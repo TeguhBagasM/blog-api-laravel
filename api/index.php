fa98c82dd7320bcdb890c8c4a5629f5ad18a6376
Teguh Bagas Mardiansyah <116806781+TeguhBagasM@users.noreply.github.com>
2025-08-08 15:58:51 +0300

    fix: deployment to vercel

--- api/index.php
+++ api/index.php
@@ -1,7 +1,3 @@
 <?php
 
-// Set the correct path for Laravel
-$_SERVER['SCRIPT_NAME'] = '/index.php';
-
-// Load Laravel
-require_once __DIR__ . '/../public/index.php';
+require __DIR__ . "../public/index.php";
