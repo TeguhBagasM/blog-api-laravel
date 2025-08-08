8225aea69b9d959d9d195b1a4610defab71c7c44
Teguh Bagas Mardiansyah <116806781+TeguhBagasM@users.noreply.github.com>
2025-08-08 14:44:34 +0300

    fix: bug error deployment

--- api/index.php
+++ api/index.php
@@ -1,4 +1,7 @@
 <?php
 
-// Redirect to Laravel public/index.php
+// Set the correct path for Laravel
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+
+// Load Laravel
 require_once __DIR__ . '/../public/index.php';
